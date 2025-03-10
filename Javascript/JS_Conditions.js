95ac07bb1bd1fc70336d6de5fcd24e07d9d93dca
Shafin Mahmood <shafin26103@gmail.com>
2025-03-10 23:19:28 +0600

    Add files via upload

---
+++ Javascript/JS_Conditions.js
@@ -0,0 +1,47 @@
+let a = 2
+let b = 3
+
+if (a==b)
+{
+    console.log("Numbers are same")
+}
+else
+{
+    console.log("numbers are not same")
+}
+
+if (typeof a=== 'number')
+{
+    console.log("The number is a number")
+}
+else
+{
+    console.log(" not a number")
+}
+
+let coldWeather = true ;
+
+if(coldWeather)
+{
+    console.log("Take the Jacket") ;
+}
+else
+{
+    console.log("Summer") ;
+}
+
+
+let array =[]
+
+if (array.length===0)
+{
+    console.log("Array is empty") ;
+}
+else 
+{
+    console.log("Array not empty") ;
+}
+
+
+
+
---
+++ Javascript/basics.js
@@ -0,0 +1,29 @@
+console.log("I am Shafin Mahmood");
+
+let firstname = 'Shafin ';
+let lastname = 'Mahmood';
+
+console.log("My Name is " +firstname+ " "+lastname);
+
+const a = 5 ; //doesn't allow re-assign
+const b = 7
+
+height = 0.5*a*b ; 
+
+console.log(height);
+
+
+let  score = 15;
+score ++ ;
+console.log(score);
+
+let run = 16;
+
+console.log(run==score);
+
+let messi = true;
+let goal = true;
+let ronaldo = true;
+
+console.log(messi && goal); 
+
