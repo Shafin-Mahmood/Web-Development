9a401a5ba18b2555e997755c82918a82459e55d5
Shafin Mahmood <shafin26103@gmail.com>
2026-03-17 15:12:16 +0600

    node learning and postman testing

---
+++ Node.JS/Express Learning and Postman Testing/index.js
@@ -0,0 +1,68 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.use(express.json());
+
+let teaData = [];
+let nextId = 1;
+
+// add a new tea
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = {
+    id: nextId++,
+    name,
+    price,
+  };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
+
+// get all teas
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
+
+// get a tea with id
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  res.status(200).send(tea);
+});
+
+// update tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+
+  res.status(200).send(tea);
+});
+
+// delete tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not found");
+  }
+
+  teaData.splice(index, 1);
+  res.status(204).send("Deleted");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
